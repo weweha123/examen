ce4876e8756a85975368fc2862ad491db6556540
weweha123 <168219216+weweha123@users.noreply.github.com>
2024-06-04 15:30:58 +0200

    First Commit

---
+++ ticket_1_15/src/main/java/edu/hneu/mjt/makarenko/ticket_1_15/Ticket115Application.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.makarenko.ticket_1_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticket115Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ticket115Application.class, args);
+    }
+
+}
---
+++ ticket_1_15/src/test/java/edu/hneu/mjt/makarenko/ticket_1_15/Ticket115ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.makarenko.ticket_1_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticket115ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
